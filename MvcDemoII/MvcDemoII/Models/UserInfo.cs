7e1c5256eb83640a3780ef2f4c022a1b665e67d3
Raj <lamaraj0007@gmail.com>
2025-05-16 11:06:01 +0545

    changes in MvcDemoII

---
+++ MvcDemoII/MvcDemoII/Models/UserInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace MvcDemoII.Models
+{
+    public class UserInfo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
